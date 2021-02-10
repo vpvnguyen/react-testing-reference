5e2ccb1314da30e41004f66d33064c4a7508fb1f
vpvnguyen <vpvnguyen@gmail.com>
2021-02-09 18:52:08 -0800

    use UsersList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { getRandomUser } from "./api/randomUser.api";
+import { UsersList } from "./components/UsersList";
 
 function App() {
-  getRandomUser();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UsersList />
     </div>
   );
 }
