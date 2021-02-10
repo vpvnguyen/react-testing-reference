044b9b01cb1b22c1177fd54fda0f3c08484e173a
vpvnguyen <vpvnguyen@gmail.com>
2021-02-09 18:54:34 -0800

    renamed to randomUsers.api.js

--- src/api/randomUser.api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const randomUserApiUrl = `https://randomuser.me/api/`;
-
-export const getRandomUser = async () => {
-  const response = await axios.get(randomUserApiUrl);
-  console.log(response.data);
-};
